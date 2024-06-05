35f336b93b894efff7206710074cd90015517aeb
HQuang0910 <134147330+ItsAzura@users.noreply.github.com>
2024-06-06 01:37:15 +0700

    done all

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -3,10 +3,59 @@ import styles from './Loading.module.css';
 
 const Loading = () => {
   return (
-    <div className={styles.loader}>
-      <div className={`${styles.inner} ${styles.one}`}></div>
-      <div className={`${styles.inner} ${styles.two}`}></div>
-      <div className={`${styles.inner} ${styles.three}`}></div>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.square}>
+          <div className={styles.square}>
+            <div className={styles.square}>
+              <div className={styles.square}>
+                <div className={styles.square}>
+                  <div className={styles.square}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.bottom}>
+        <div className={styles.square}>
+          <div className={styles.square}>
+            <div className={styles.square}>
+              <div className={styles.square}>
+                <div className={styles.square}>
+                  <div className={styles.square}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.left}>
+        <div className={styles.square}>
+          <div className={styles.square}>
+            <div className={styles.square}>
+              <div className={styles.square}>
+                <div className={styles.square}>
+                  <div className={styles.square}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.square}>
+          <div className={styles.square}>
+            <div className={styles.square}>
+              <div className={styles.square}>
+                <div className={styles.square}>
+                  <div className={styles.square}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
